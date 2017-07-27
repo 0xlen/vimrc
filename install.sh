964343289774faedbf708eeccb993b61dd7548fd
Len <a78965@gmail.com>
2017-07-28 07:19:28 +0800

    Fix bug when linking the symbolic link in install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ln -s .vimrc ~/.vimrc
-ln -s .vim ~/.vim
+ln -s `pwd`/.vimrc ~/.vimrc
+ln -s `pwd`/.vim ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git .vim/bundle/Vundle.vim
 vim +PluginInstall +qall
