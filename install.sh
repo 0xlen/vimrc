aa18a92a85c820902ea83ed802da55a38174ecca
Eason Cao <a78965@gmail.com>
2017-08-16 17:12:30 +0800

    Add install Exuberant ctags for tagbar in install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@ ln -s `pwd`/.vimrc ~/.vimrc
 ln -s `pwd`/.vim ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git .vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+
+# Install Exuberant ctags for tagbar
+sudo apt-get install -y ctags
