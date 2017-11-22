f3db94df03aabef3ec310d607d66d173cdd08e68
Eason Cao <a78965@gmail.com>
2017-11-22 08:18:50 +0000

    Fix the path bug in install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ln -s `pwd`/.vimrc ~/.vimrc
 ln -s `pwd`/.vim ~/.vim
-git clone https://github.com/VundleVim/Vundle.vim.git .vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
 # Install Exuberant ctags for tagbar
